390987ac75c99ea0c9dbf974588bd778b77ed90b
Ruud Visser <visser.rgm@gmail.com>
2022-11-07 11:57:28 +0100

    Setup mantine plus remix

---
+++ app/root.tsx
@@ -0,0 +1,53 @@
+import type { LinksFunction, LoaderArgs, MetaFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
+import { MantineProvider, createEmotionCache } from "@mantine/core";
+import { StylesPlaceholder } from "@mantine/remix";
+import tailwindStylesheetUrl from "./styles/tailwind.css";
+import { getUser } from "./session.server";
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: tailwindStylesheetUrl }];
+};
+
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "Moderation Prototype",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+createEmotionCache({ key: "mantine" });
+
+export async function loader({ request }: LoaderArgs) {
+  return json({
+    user: await getUser(request),
+  });
+}
+
+export default function App() {
+  return (
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <html lang="en" className="h-full">
+        <head>
+          <StylesPlaceholder />
+          <Meta />
+          <Links />
+        </head>
+        <body className="h-full">
+          <Outlet />
+          <ScrollRestoration />
+          <Scripts />
+          <LiveReload />
+        </body>
+      </html>
+    </MantineProvider>
+  );
+}
